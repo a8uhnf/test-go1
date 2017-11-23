c41860bae48f3d8e9ae88aa762a0890fe9368f1e
hanif <hanif@appscode.com>
2017-11-23 16:59:34 +0600

    Update

--- hello_world.go
+++ hello_world.go
@@ -7,10 +7,10 @@ import (
 
 func HelloWorld() {
 	fmt.Println("Hello World from test-go1. branch: test-dep1")
-	a.HelloWorldTestDep()
+	a.HelloWorldMaster()
 }
 
 func HelloWorldNewWorld()  {
 	fmt.Println("Hello World from New World!!!!! Branch: test-dep1")
-	a.HelloWorldTestDep()
+	a.HelloWorldMaster()
 }
